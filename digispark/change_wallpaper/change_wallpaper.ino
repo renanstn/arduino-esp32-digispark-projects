8af4a07dd0fea4e87a91b8c8019f4939d6a337bd
Renan Santana <renan_ota@hotmail.com>
2021-10-26 18:30:57 -0300

    ajusta script com wallpaper do vampeta

--- digispark/change_wallpaper/change_wallpaper.ino
+++ digispark/change_wallpaper/change_wallpaper.ino
@@ -13,11 +13,10 @@ void loop() {
 
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(100);
-  DigiKeyboard.println("powershell Set-ItemProperty -path 'HKCU:\\Control Panel\\Desktop\\' -name wallpaper -value \"C:\\Users\\renan\\Pictures\\wp_test.jpg\";rundll32.exe user32.dll, UpdatePerUserSystemParameters;");
+  DigiKeyboard.println("powershell Set-ItemProperty -path 'HKCU:\\Control Panel\\Desktop\\' -name wallpaper -value \"C:\\Users\\renan\\Pictures\\vampeta.jpg\";rundll32.exe user32.dll, UpdatePerUserSystemParameters;");
   digitalWrite(LED_PIN, HIGH);
   for(;;);
 }
 
-// powershell Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "C:\Users\renan\Pictures\wp_test.jpg";rundll32.exe user32.dll, UpdatePerUserSystemParameters;
-
-// C:\Users\renan\Documents\ArduinoData\packages\digistump\hardware\avr\1.6.7\libraries\DigisparkKeyboard\scancode-ascii-table.h
+// Comando sem os caracteres de escape, somente para referência
+// powershell Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value "C:\Users\renan\Pictures\vampeta.jpg";rundll32.exe user32.dll, UpdatePerUserSystemParameters;
